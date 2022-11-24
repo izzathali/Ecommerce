374d341d3cc46c8257dc5ea9cd4ca14ae9c57f70
Izzath Ali <izzath.info@gmail.com>
2022-11-24 13:25:15 +0530

    Add project files.

---
+++ Ecommerce.Model/CategoryM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Ecommerce.Model
+{
+    public class CategoryM
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        [Display(Name = "Category Name")]
+        public string CategoryName { get; set; } = string.Empty;
+        public bool IsDeleted { get; set; } = false;
+    }
+}
